0ed2e6d165d7497e8bbe1152f0e094f3064916a4
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-02-15 22:15:31 +0300

    Odev1

--- Odev1.sql
+++ Odev1.sql
@@ -4,7 +4,7 @@ SELECT title, description FROM film;
 
 --2)film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
 SELECT * FROM film 
-WHERE between 60 and 75;
+WHERE length>60 AND length<75;
 
 
 --3)film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
@@ -19,5 +19,5 @@ WHERE rental_rate=0.99 and  replacement_cost=12.99 or  replacement_cost=28.99;
   
   --5)film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
   SELECT *FROM film
-  WHERE length<=50 and rental_rate not in (2.99,4.99);
+  WHERE length<=50 and rental_rate <>2.99 OR rental_rate<>4.99;
   
