d1a1ce3f9318e32c5b5221edd5e44b57f1bebb2d
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-02-15 22:30:28 +0300

    Odev1

--- Odev1.sql
+++ Odev1.sql
@@ -19,5 +19,5 @@ WHERE rental_rate=0.99 and  replacement_cost=12.99 or  replacement_cost=28.99;
   
   --5)film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
   SELECT *FROM film
-  WHERE length<=50 and rental_rate <>2.99 OR rental_rate<>4.99;
+  WHERE length<=50 and (rental_rate <>2.99 OR rental_rate<>4.99);
   
