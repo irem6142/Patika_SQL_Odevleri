c089aa65898e1771c77b7bdbf13a54e696209c94
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-02-18 17:44:15 +0300

    Odev2

--- Odev2.sql
+++ Odev2.sql
@@ -12,4 +12,4 @@ WHERE first_name IN('Penelope','Nick','Ed');
 --3)film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
 SELECT *
 FROM film
-WHERE  rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost(12.99, 15.99, 28.99);
+WHERE  rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
