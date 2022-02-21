bac7624824414eacecbfe0005c03039f7e945eb9
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-02-21 22:10:07 +0300

    Odev8

---
+++ Odev8.sql
@@ -0,0 +1,103 @@
+--1)test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+CREATE TABLE employe(
+       id INTEGER PRIMAY KEY,
+       name VARCHAR(50) NOT NULL,
+       birthday DATE NOT NULL,
+       email VARCHAR(100)
+      );
+
+
+--2)Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+insert into employee (name, email, birthday) values ('kflowith0', 'iconstantine0@ucoz.com', '1976-03-05');
+insert into employee (name, email, birthday) values ('zdarwent1', 'cdendon1@unblog.fr', '1980-05-21');
+insert into employee (name, email, birthday) values ('ccaslake2', 'bandriveaux2@mail.ru', '1980-11-07');
+insert into employee (name, email, birthday) values ('isurgood3', 'amccutcheon3@zimbio.com', '1994-12-05');
+insert into employee (name, email, birthday) values ('sreason4', 'mcasado4@ibm.com', '1985-02-25');
+insert into employee (name, email, birthday) values ('abalstone5', null, '1999-07-22');
+insert into employee (name, email, birthday) values ('adymond6', 'ekynd6@weather.com', '1991-08-15');
+insert into employee (name, email, birthday) values ('erock7', 'brangeley7@php.net', '1986-06-10');
+insert into employee (name, email, birthday) values ('mguilayn8', 'hfutcher8@java.com', '1983-09-29');
+insert into employee (name, email, birthday) values ('alitton9', 'lkilcullen9@state.gov', '1987-05-21');
+insert into employee (name, email, birthday) values ('ehourstona', 'pbullanta@weebly.com', '1977-10-06');
+insert into employee (name, email, birthday) values ('kmcconnalb', null, '1996-02-06');
+insert into employee (name, email, birthday) values ('mwadwellc', 'dmelleyc@un.org', '1994-10-18');
+insert into employee (name, email, birthday) values ('mcowelld', 'jsaywardd@hp.com', '1998-03-20');
+insert into employee (name, email, birthday) values ('kwiffene', 'kkamane@domainmarket.com', '1977-08-17');
+insert into employee (name, email, birthday) values ('blindenf', 'tcavanf@youtu.be', '1980-06-20');
+insert into employee (name, email, birthday) values ('lfarmarg', 'rchivertong@biglobe.ne.jp', '1997-01-13');
+insert into employee (name, email, birthday) values ('bkhilkovh', 'waltoftsh@smh.com.au', '1986-02-16');
+insert into employee (name, email, birthday) values ('fgooddiei', 'jmoeni@instagram.com', '1998-06-18');
+insert into employee (name, email, birthday) values ('cvanhaultj', null, '1996-03-16');
+insert into employee (name, email, birthday) values ('cschrinelk', 'jlascellk@unc.edu', '1992-10-01');
+insert into employee (name, email, birthday) values ('cadkinl', 'mgarriochl@yahoo.com', '1992-01-01');
+insert into employee (name, email, birthday) values ('foccleshawm', 'mnillesm@microsoft.com', '1990-07-05');
+insert into employee (name, email, birthday) values ('fwadlown', 'mclausenn@nasa.gov', '1999-09-02');
+insert into employee (name, email, birthday) values ('imccuskero', 'sarchambaulto@bbc.co.uk', '1977-05-02');
+insert into employee (name, email, birthday) values ('gfergysonp', 'fmcbeithp@newsvine.com', '1978-10-05');
+insert into employee (name, email, birthday) values ('cdarridonq', 'amorenaq@ask.com', '1998-06-16');
+insert into employee (name, email, birthday) values ('aweippertr', 'cdaelr@lulu.com', '1985-03-23');
+insert into employee (name, email, birthday) values ('mmolfinos', 'kdionisos@wiley.com', '1984-06-25');
+insert into employee (name, email, birthday) values ('swoodburnt', 'tpetegret@gmpg.org', '1996-06-11');
+insert into employee (name, email, birthday) values ('enevittu', 'gheveyu@mlb.com', '1985-04-22');
+insert into employee (name, email, birthday) values ('bnorquoyv', 'ssandemanv@reverbnation.com', '1986-03-14');
+insert into employee (name, email, birthday) values ('sguilaynw', 'hgoodayw@unblog.fr', '1977-06-27');
+insert into employee (name, email, birthday) values ('vdunbobinx', 'ltrodlerx@cdc.gov', '1993-08-11');
+insert into employee (name, email, birthday) values ('lbloany', 'klinnelly@ox.ac.uk', '1996-10-28');
+insert into employee (name, email, birthday) values ('mcatenotz', 'schampneysz@walmart.com', '1980-11-30');
+insert into employee (name, email, birthday) values ('denochsson10', 'mcarling10@un.org', '1994-08-14');
+insert into employee (name, email, birthday) values ('dcadore11', 'anoel11@exblog.jp', '1985-01-26');
+insert into employee (name, email, birthday) values ('agotling12', 'gleverage12@github.io', '1982-12-15');
+insert into employee (name, email, birthday) values ('aclynter13', 'ktanzer13@fema.gov', '1982-03-23');
+insert into employee (name, email, birthday) values ('cseager14', 'nschiementz14@wisc.edu', '1981-10-26');
+insert into employee (name, email, birthday) values ('sfante15', 'mstogill15@buzzfeed.com', '1995-11-17');
+insert into employee (name, email, birthday) values ('alomb16', 'asemerad16@webs.com', '1986-04-30');
+insert into employee (name, email, birthday) values ('cniesing17', 'vbilham17@netlog.com', '1996-01-15');
+insert into employee (name, email, birthday) values ('ehavvock18', 'malloisi18@reverbnation.com', '1976-09-09');
+insert into employee (name, email, birthday) values ('ssowley19', 'emackeogh19@theguardian.com', '1982-08-22');
+insert into employee (name, email, birthday) values ('kmarzella1a', 'qspinige1a@newyorker.com', '1992-02-13');
+insert into employee (name, email, birthday) values ('ddominighi1b', 'dtuiller1b@icio.us', '1998-08-01');
+insert into employee (name, email, birthday) values ('opennino1c', 'cgoodinson1c@indiatimes.com', '1989-12-20');
+insert into employee (name, email, birthday) values ('ppates1d', 'ckynder1d@myspace.com', '1983-08-12');
+
+ 
+ --3)Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+ UPDATE employee
+ SET name='irem',
+     email='irem1453@gmail.com'
+  WHERE id  BETWEEN 1 AND 5; 
+  
+  
+  --4)Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+  DELETE FROM employee
+  WHERE id>45;
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
